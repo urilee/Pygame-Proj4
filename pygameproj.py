d8062dc7bd69d2bbaa5dcbd61b961eca9e0b01a4
Uriel Lee <uriellee@umich.edu>
2016-11-16 12:36:40 -0500

    small things

--- pygameproj.py
+++ pygameproj.py
@@ -1,2 +1,12 @@
 #pygame file
 #need to start thinking of pygame ideas
+import random
+import sys
+import pygame
+from pygame.locals import Rect, #different keys and stuff to help make coding a lot easier
+
+#dimensions of the game
+X_MAX = 0
+Y_MAX = 0
+everything = pygame.sprite.Group()
+#groups are more like dictionary 
