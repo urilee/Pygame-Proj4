40b31cc7bb111b801d423270b34be0297a631b5b
Uriel Lee <uriellee@umich.edu>
2016-12-02 16:23:25 -0500

    final changes

--- pygameproj.py
+++ pygameproj.py
@@ -1,15 +1,15 @@
 #Breakout version by Uriel Lee
 
-import random
 import sys
 import pygame
 import os
 
+#these are all constants
 #dimensions for display screen
 display_screen = screen_w, screen_h = 600, 600
 screen = pygame.display.set_mode(display_screen)
 
-#constants
+#game state constants
 intro = 0
 ball_in_paddle = 1
 playing = 2
@@ -19,7 +19,9 @@ game_over = 4
 #images for the background
 background_image = (pygame.image.load(os.path.join('images','start.png')), pygame.image.load(os.path.join('images','game.png')),pygame.image.load(os.path.join('images','game2.png')), pygame.image.load(os.path.join('images','won.png')), pygame.image.load(os.path.join('images','lose.png')))
 
+
 class Colors:
+
 	ball_color = (255, 255, 255) #white
 	paddle_color = (139, 137, 137) #snow
 
@@ -33,6 +35,8 @@ class Paddle(pygame.sprite.Sprite):
 		
 		self.y = display_screen[0] - 70
 		self.rect = pygame.Rect(200, self.y, self.width, self.height) #left, top, width, height
+
+		#initial x and y position
 		self.rect.x = 200
 		self.rect.y = self.y
 		self.max_paddle = display_screen[0] - self.width
@@ -42,22 +46,27 @@ class Bricks(pygame.sprite.Sprite):
 	def __init__(self):
 		self.img = pygame.image.load(os.path.join('images','brick.png'))
 		self.rect = self.img.get_rect()
+		#dimensions
 		self.l = self.rect.right - self.rect.left
 		self.h = self.rect.bottom - self.rect.top
 
 class Ball(pygame.sprite.Sprite):
 
 	def __init__(self):
-		self.y = display_screen[0] - 70 #sets how high the paddle is
 		self.diameter = 20
 		self.radius = int(self.diameter / 2)
 		self.max_ball_x = display_screen[0] - self.diameter
 		self.max_ball_y = display_screen[1] - self.diameter
 
+		self.y = display_screen[0] - 70 #how high the paddle is
 		self.rect = pygame.Rect(200, self.y - self.diameter, self.diameter, self.radius)
+
+		#initial x and y position
 		self.rect.x = 200
 		self.rect.y = self.y
-		self.v = [4,-4]
+
+		#x and y velocity
+		self.v = [6,-6]
 
 class Game(pygame.sprite.Sprite):
 
@@ -69,6 +78,8 @@ class Game(pygame.sprite.Sprite):
 		self.lives = 3
 		self.score = 0
 		self.state = intro
+
+		#sounds
 		self.sound = pygame.mixer.Sound(os.path.join('sounds','power.wav'))
 		self.sound_lose = pygame.mixer.Sound(os.path.join('sounds','sad.wav'))
 		self.sound_win = pygame.mixer.Sound(os.path.join('sounds','yay.wav'))
@@ -106,12 +117,12 @@ class Game(pygame.sprite.Sprite):
 
 		#paddle movement
 		if keys[pygame.K_LEFT]:
-			self.paddle.rect.x -= 5
+			self.paddle.rect.x -= 8
 			if self.paddle.rect.x < 0:
 				self.paddle.rect.x = 0
 
 		if keys[pygame.K_RIGHT]:
-			self.paddle.rect.x += 5
+			self.paddle.rect.x += 8
 			if self.paddle.rect.x > self.paddle.max_paddle:
 				self.paddle.rect.x = self.paddle.max_paddle
 	
@@ -126,12 +137,18 @@ class Game(pygame.sprite.Sprite):
 		#press ESC to end the game
 		elif keys[pygame.K_ESCAPE]:
 			self.state = game_over
+		
+		#press q to completely quite out of the game	
+		elif keys[pygame.K_q]:
+			pygame.quit()
+			sys.exit()
 	
-	def move_ball(self):
+	def ball_movement(self):
 		self.ball.rect.x += self.ball.v[0] #x direction
 		self.ball.rect.y  += self.ball.v[1] #y direction
 
-		#makes sure that the ball stays in boundaries
+		#makes sure that the ball stays in boundaries - reverses direction when hits wall
+		#x position
 		if self.ball.rect.x <= 0:
 			self.ball.rect.x = 0
 			self.ball.v[0] = -self.ball.v[0]
@@ -140,6 +157,7 @@ class Game(pygame.sprite.Sprite):
 			self.ball.rect.x = self.ball.max_ball_x
 			self.ball.v[0] = -self.ball.v[0]
 
+		#y position
 		if self.ball.rect.y < 0:
 			self.ball.rect.y = 0
 			self.ball.v[1] = -self.ball.v[1]
@@ -148,7 +166,7 @@ class Game(pygame.sprite.Sprite):
 			self.ball.rect.y = self.ball.max_ball_y
 			self.ball.v[1] = -self.ball.v[1]
 
-	def handle_collisions(self):
+	def collisions(self):
 		#when ball hits bricks - removes bricks - reverses direction
 		for brick in self.lists:
 			if self.ball.rect.colliderect(brick):
@@ -158,7 +176,7 @@ class Game(pygame.sprite.Sprite):
 				self.lists.remove(brick)
 				break
 
-		#no more bricks in the list
+		#no more bricks in the list - winner
 		if len(self.lists) == 0:
 			self.state = won
 
@@ -185,36 +203,42 @@ class Game(pygame.sprite.Sprite):
 		scorerect.centerx = screen.get_rect().centerx #places score at the top center
 		screen.blit(score, scorerect)
 
-	def show_text(self):
+	def show_esc(self):
 		font = pygame.font.Font(None, 20)
 		text = font.render("press ESC to end game", True, (Colors.ball_color))
 		textrect = text.get_rect()
 		screen.blit(text, (445, 580))
+
+	def show_quit(self):
+		font = pygame.font.Font(None, 20)
+		text = font.render("press Q to QUIT", True, (Colors.ball_color))
+		textrect = text.get_rect()
+		screen.blit(text, (480, 580))
 
 	def status_of_game(self):
 		screen.blit(background_image[self.state], (0,0, screen_w, screen_h), (0,0,screen_w, screen_h))
 		
 		if self.state == ball_in_paddle or self.state == playing:
 			self.show_score()
-			self.show_text()
-			self.move_ball()
-			self.handle_collisions()
+			self.show_esc()
+			self.ball_movement()
+			self.collisions()
 			self.draw_things()
+			pygame.display.set_caption("KILL THE ALIENS!!")
 
 		elif self.state == intro:
+			pygame.display.set_caption("BREAKOUT")
 			pygame.mixer.stop()
-			pass
 
 		elif self.state == game_over:
 			self.show_score()
 			self.sound_lose.play()
-			pass
+			pygame.display.set_caption("TRY AGAIN?")
 
 		elif self.state == won:
 			self.show_score()
 			self.sound_win.play()
-			pass
-
+			pygame.display.set_caption("CONGRATS!!")
 
 	def run(self):
 		while 1:
@@ -224,8 +248,8 @@ class Game(pygame.sprite.Sprite):
 
 			self.play_game()
 			self.status_of_game()
+			self.show_quit()
 
-			pygame.display.set_caption("BREAKOUT")
 			pygame.display.flip()
 
 if __name__ == "__main__":
